06a77b2f78efb196d447f9cfafd71c6c4aec4fa5
katsumaru <44katsu26@gmail.com>
2021-12-30 15:09:47 +0900

    フレーム

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+
+</head>
+
+<body>
+  <header>
+
+  </header>
+  <div class="main-wrapper">
+    <h2>ツールボックス</h2>
+  </div><!-- /main-wrapper -->
+  <footer>
+
+  </footer>
+</body>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+</html>
