8eff6b82a9b09d4310e13cd339de4b0b851eeb99
katsumaru <44katsu26@gmail.com>
2021-12-30 16:50:14 +0900

    修正

--- common/header.php
+++ common/header.php
@@ -1,5 +1,5 @@
 <div class="header-contents">
-  <h1><?= $hederTitle ?></h1>
+  <h1><?= $headerTitle ?></h1>
   <h2>便利ツールボックス</h2>
   <div class="btn" id="open_btn">
     <label class="menu-btn"><span></span></label>
--- index.php
+++ index.php
@@ -1,21 +1,48 @@
 <!DOCTYPE html>
-<html lang="ja">
+<html lang="ja" prefix="og:http://ogp.me/ns#">
 
 <head>
-  <?php $title = "お仕事ツールボックス" ?>
-  <?php require_once "../common/head.php"; ?>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <meta http-equiv="x-ua-compatible" content="IE=edge">
+  <title>WEBツールボックス</title>
+  <meta name="description" content="仕事ツール等の便利なWEBツールサイトです">
+  <meta name="keywords" content="WEBツール,プログラミング,便利ボックス">
+  <link href="/css/style.css" rel="stylesheet" type="text/css">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+  <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="192x192" href="android-touch-icon.png">
+  <meta property="og:title" content="仕事ツール">
+  <meta property="og:type" content="website">
+  <meta property="og:description" content="仕事ツール等の便利なWEBツールサイトです">
+  <meta property="og:url" content="https://tool.katsumaru.blog/">
+  <meta property="og:site_name" content="WEBツール">
+  <meta property="og:image" content="https://tool.katsumaru.blog/images/tool.jpg">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta name="apple-mobile-web-app-status-bar-style" content="black">
+  <meta name="apple-mobile-web-app-title" content="WEBツール">
+  <link rel="apple-touch-icon" href="apple-touch-icon.png">
+  <link rel="manifest" href="manifest.json">
+  <script>
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('sw.js')
+        .then((reg) => {
+          console.log('Service worker registered.', reg);
+        });
+    }
+  </script>
 </head>
 
 <body>
   <header>
-    <?php $hederTitle = "お仕事ツールボックス" ?>
-    <?php require_once "../common/header.php"; ?>
+    <?php $headerTitle = "便利なWEBツールボックス" ?>
+    <?php require_once "common/header.php"; ?>
   </header>
   <div class="main-wrapper">
-    <h2>ツールボックス</h2>
+    <h2>今後追加予定</h2>
   </div><!-- /main-wrapper -->
   <footer>
-    <?php require_once "../common/footer.php"; ?>
+    <?php require_once "common/footer.php"; ?>
   </footer>
 </body>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
