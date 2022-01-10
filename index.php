7370d1e3d842aec691e16ada04d61e7cbfc17a3e
katsumaru <44katsu26@gmail.com>
2022-01-10 17:06:51 +0900

    フレーム仮

--- index.php
+++ index.php
@@ -42,9 +42,14 @@
   <div class="wall">
     <div class="main-wrapper">
       <section>
-        <a href="content/rate.php"><img src="images/saiken.png" alt="債権係数"></a>
-        <a href="content/dollarRate.php">円ドル変換ツール</a>
-
+        <div class="column">
+          <div class="clm3">
+            <a href="content/rate.php"><img src="images/saiken.png" alt="債権係数"></a>
+          </div>
+          <div class="clm3">
+            <a href="content/dollarRate.php"><img src="images/yendol.png" alt="円ドル変換"></a>
+          </div>
+        </div><!-- /column -->
       </section>
     </div><!-- /main-wrapper -->
   </div><!-- /wall -->
