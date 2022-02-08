8209d6707092665890bccae6d2be8a457de90201
trust <44katsu26@gmail.com>
2022-02-08 13:36:56 +0900

    write修正

--- private/writeData.php
+++ private/writeData.php
@@ -8,19 +8,12 @@ if (empty($_POST["csvData"])) {
 
 $csvData = $_POST["csvData"];
 $filename = "../data/covid_2022.csv";
-try {
-  $fileobj = new SplFileObject($filename, "ab");
-} catch (Exception $e) {
-  echo '<span class="error">エラーがありました</span>';
-  echo $e->getMessage();
-  exit();
-}
-
-$fileobj->flock(LOCK_EX); // 排他ロック
-$result = $fileobj->fwrite($csvData); //メモを追記
-$fileobj->flock(LOCK_UN); //アンロック
+$fp = fopen($filename, "ab");
+flock($fp, LOCK_EX);
+fwrite($fp, $csvData . "\n");
+flock($fp, LOCK_UN);
 
 //リダイレクト
-$url = "https://tool.katsumaru.blog/content/covid2022.php";
+$url = "http://localhost/develop/tool/content/covid2022.php";
 header("Location:" . $url);
 exit();
