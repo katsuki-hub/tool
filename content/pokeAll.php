c95f8a952742b09114830e9e648a21a490002c2c
trust <44katsu26@gmail.com>
2022-02-17 16:01:09 +0900

    all

--- content/pokeAll.php
+++ content/pokeAll.php
@@ -2,10 +2,10 @@
 require_once("../common/es.php");
 $backURL = "pokemon.php";
 //接続パラメーター
-$user = 'katsuki';
+$user = 'LAA1192529';
 $passwoed = 'katsu4426';
-$dbName = 'pokemon';
-$host = 'localhost:3306';
+$dbName = 'LAA1192529-tool';
+$host = 'mysql202.phy.lolipop.lan';
 $dsn = "mysql:host={$host};dbname={$dbName};charset=utf8";
 ?>
 
