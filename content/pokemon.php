38e1b9d59c13d7d9cd2e91a074653806a0e95af9
katsumaru <44katsu26@gmail.com>
2022-02-18 21:22:03 +0900

    フォーム

--- content/pokemon.php
+++ content/pokemon.php
@@ -4,6 +4,12 @@
 <head>
   <?php $title = "ポケモン図鑑" ?>
   <?php require_once "../common/head.php"; ?>
+  <style>
+    .all input[type="submit"]:valid {
+      font-size: 20px;
+      font-weight: bold;
+    }
+  </style>
 </head>
 
 <body>
@@ -16,17 +22,18 @@
       <article>
         <section>
           <h2>ポケモン図鑑</h2>
-          
+          <div class="br30"></div>
           <form method="POST" action="nameSearch.php">
             <ul>
               <li>
-                <label>ポケモンを検索：<br>
-                  <input type="text" name="name" placeholder="ポケモンを入力">
+                <label>名前で検索：<br>
+                  <input type="text" name="name" placeholder="なまえを入力">
                 </label>
               </li>
               <li><input type="submit" value="検索する"></li>
             </ul>
           </form><br>
+          <hr>
 
           <form method="POST" action="pokeSearch.php">
             <ul>
@@ -34,30 +41,47 @@
                 <span>タイプで検索：</span><br>
                 <select name="type">
                   <option value="ノーマル">ノーマル</option>
-                  <option value="くさ">くさ</option>
                   <option value="ほのお">ほのお</option>
                   <option value="みず">みず</option>
+                  <option value="くさ">くさ</option>
                   <option value="ひこう">ひこう</option>
                   <option value="どく">どく</option>
                   <option value="むし">むし</option>
                   <option value="でんき">でんき</option>
                   <option value="じめん">じめん</option>
-                  <option value="フェアリー">フェアリー</option>
                   <option value="かくとう">かくとう</option>
-                  <option value="いわ">いわ</option>
                   <option value="エスパー">エスパー</option>
-                  <option value="はがね">はがね</option>
                   <option value="こおり">こおり</option>
+                  <option value="いわ">いわ</option>
                   <option value="ゴースト">ゴースト</option>
                   <option value="ドラゴン">ドラゴン</option>
+                  <option value="あく">あく</option>
+                  <option value="はがね">はがね</option>
+                  <option value="フェアリー">フェアリー</option>
                 </select>
               </li>
               <li><input type="submit" value="検索する"></li>
             </ul>
-          </form>
-          <div class="br50"></div>
+          </form><br>
+          <hr>
 
-          <form action="pokeAll.php" style="text-align: center;">
+          <p>※後日実装します。現在未対応</p>
+          <form method="POST" action="">
+            <ul>
+              <li>
+                <span>地方で検索：</span><br>
+                <select name="tihou">
+                  <option value="カントー">カントー</option>
+                  <option value="ジョウト">ジョウト</option>
+                </select>
+              </li>
+              <li><input type="submit" value="検索する"></li>
+            </ul>
+          </form><br>
+          <hr>
+          <div class="br30"></div>
+
+          <form action="pokeAll.php" style="text-align: center;" class="all">
             <input type="submit" value="全ポケモン表示">
           </form><br>
 
