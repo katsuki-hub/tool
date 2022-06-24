0bfb8078b962fa59c22e9ba86b17c29627c7d838
trust <44katsu26@gmail.com>
2022-06-24 13:36:35 +0900

    テスト

--- content/theoretical_stock.php
+++ content/theoretical_stock.php
@@ -13,22 +13,27 @@
   </header>
   <div class="wall">
     <div class="main-wrapper">
-      <article>
-        <section>
-          <h2>大診断拡張版2022夏号</h2>
-          <div class="br30"></div>
-          <form method="POST" action="theoretical_data.php">
-            <ul>
-              <li>
-                <label>株価診断<br>
-                  <input type="text" name="code" placeholder="証券コード or 社名">
-                </label>
-              </li>
-              <li><input type="submit" value="検索する"></li>
-            </ul>
-          </form>
-        </section>
-      </article>
+      <section>
+        <h2>大診断拡張版2022夏号</h2>
+        <div class="br30"></div>
+        <form method="POST" action="theoretical_data.php">
+          <ul>
+            <li>
+              <label>株価診断<br>
+                <input type="text" name="code" placeholder="証券コード or 社名">
+              </label>
+            </li>
+            <li><input type="submit" value="検索する"></li>
+          </ul>
+        </form>
+        <div class="br30"></div>
+        <div class="frame1">
+          理論株価とは、会社が持っている価値（企業価値）を元に計算された株価です。<br>
+          その会社の「あるべき株価」とも言えます。<br>
+          PERと同じように、投資判断に使える指標(割安性)に使えますが、理論株価は会社の価値を元に計算しているので、PERよりも厳密に割安性が測れます。
+        </div>
+        ※四半期に渡って更新予定！！現在の指標は2022年6月8日データ
+      </section>
     </div><!-- /main-wrapper -->
   </div><!-- /wall -->
   <?php require_once "../common/footer.php"; ?>
